61f2eb59febe9c875cba366d25402340d300231a
CESGomes <cesgomes@gmail.com>
2024-09-22 20:00:31 -0300

    Logica e mensagem para log in e log out

--- store/views.py
+++ store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
 from django.contrib.auth import authenticate, login, logout 
 from django.contrib import messages
@@ -12,7 +12,21 @@ def about(request):
     return render(request, 'about.html',{} )
 
 def login_user(request):
-    return render(request, 'login.html', {})
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username = username, password= password)
+        if user is not None:
+            login(request, user)
+            messages.success(request,"You have been logged in, Welcome aboard")
+            return redirect("home")
+        else:
+            messages.error(request, "There was an error, please try again")
+            return redirect("login")
+    else:
+        return render(request, 'login.html', {})
 
 def logout_user(request):
-    pass
+    logout(request)
+    messages.success(request,("You have been logged out"))
+    return redirect('home')
