d14e2cb64210abbdcc7beef36113626d56a9cf12
Carlos Eduardo Gomes <cesgomes@gmail.com>
2024-09-28 02:12:05 -0300

    Criação do modelo de Profile

--- store/models.py
+++ store/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 import datetime
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
@@ -38,3 +40,18 @@ class Order(models.Model):
 
     def __str__(self):
         return f'{self.product} - {self.customer}'
+
+#Create Customer Profile
+class Profile(models.Model):
+    user = Models.models.OneToOneField(User, on_delete=models.CASCADE)
+    date_modified = models.DateTimeField(User,auto_now=True)
+    phone = models.CharField(max_length=20, blank=True)
+    address1 = models.CharField(max_length=200, blank=True)
+    address2 = models.CharField(max_length=200, blank=True)
+    city = models.CharField(max_length=50, blank=True)
+    state = models.CharField(max_length=20, blank=True)
+    zipcode = models.CharField(max_length=15, blank=True)
+    country = models.CharField(max_length=20, blank=True)
+
+    def __str__(self):
+        return self.user.username
