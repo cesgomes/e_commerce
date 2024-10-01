15cc94df5ebb3a27841970a6350346a738ec5c54
Carlos Eduardo Gomes <cesgomes@gmail.com>
2024-10-01 12:49:22 -0300

    Configuração de uma pagina dummy para o novo app

--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def payment_success(request):
+    """
+    Handles the successful payment response.
+
+    Args:
+        request: The HTTP request object.
+
+    Returns:
+        [HttpResponse](https://docs.djangoproject.com/en/4.2/ref/request-response/#django.http.HttpResponse): Renders the payment success HTML page.
+    """
+    return render(request, "payment/payment_success.html", {})
