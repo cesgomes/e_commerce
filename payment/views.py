a612853ce6819cde4ccc1326c3a2cd3c1824ef0a
Carlos Eduardo Gomes <cesgomes@gmail.com>
2024-10-02 13:09:24 -0300

    Billing Address

--- payment/views.py
+++ payment/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from cart.cart import Cart
+from .forms import ShippingForm, ShippingAddress
 
 def payment_success(request):
     """
@@ -19,7 +20,15 @@ def checkout(request):
     cart_products = cart.get_prods
     quantities = cart.get_quants
     totals = cart.cart_total()
+    
+    if request.user.is_authenticated:
+        #Checkout as user
+        shipping_user = ShippingAddress.objects.get(user__id=request.user.id)
+        shipping_form = ShippingForm(request.POST or None, instance=shipping_user)
+    else:
+        #Checkout as guest
+        shipping_form = ShippingForm(request.POST or None)
     return render(request, "payment/checkout.html", {"cart_products": cart_products,
-                                                 "quantities": quantities,
-                                                 "totals": totals})
-
+                                                    "quantities": quantities,
+                                                    "shipping_form": shipping_form,
+                                                    "totals": totals})
