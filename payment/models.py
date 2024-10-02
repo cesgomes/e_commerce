29e1fb686f930cb6a241991b402297c60c5fa972
Carlos Eduardo Gomes <cesgomes@gmail.com>
2024-10-02 00:25:05 -0300

    Modelo dos pedidos (header)

--- payment/models.py
+++ payment/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from store.models import Product
 class ShippingAddress(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE, null=True, blank=True)
     shipping_full_name=models.CharField(max_length=255)
@@ -19,3 +19,17 @@ class ShippingAddress(models.Model):
     def __str__(self):
         return f'Shipping Address - {str(self.id)}'
     
+# Create order Model (header)
+class Order(models.Model):
+    #Foreign key
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    full_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    shipping_address = models.CharField(max_length=1000)
+    amount_paid = models.DecimalField(max_digits=7, decimal_places=2)
+    date_ordered = models.DateField(auto_now_add=True)
+    
+    def __str__(self):
+        return f'Order - {str(self.id)}'
+    
+# Create Order Items Model
