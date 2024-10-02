28ada12cf6392351e175ba529535d24db984e8da
Carlos Eduardo Gomes <cesgomes@gmail.com>
2024-10-02 11:29:35 -0300

    3 pilares da página de Checkout e ajustes no carrinho

--- payment/urls.py
+++ payment/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+# Definindo as URLs do aplicativo
 urlpatterns = [
+    # URL para a página de sucesso do pagamento
     path('payment_success/', views.payment_success, name="payment_success"),
+
+    # URL para a página de checkout
+    path('checkout/', views.checkout, name="checkout"),
 ]
--- payment/views.py
+++ payment/views.py
@@ -12,3 +12,15 @@ def payment_success(request):
         [HttpResponse](https://docs.djangoproject.com/en/4.2/ref/request-response/#django.http.HttpResponse): Renders the payment success HTML page.
     """
     return render(request, "payment/payment_success.html", {})
+
+def checkout(request):
+    """
+    Handles the checkout process by rendering the checkout page.
+
+    Args:
+        request: The HTTP request object.
+
+    Returns:
+        HttpResponse: The rendered checkout page.
+    """
+    return render(request, "payment/checkout.html", {})
