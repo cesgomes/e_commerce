41b6dd226efad2653505ae4cd5925169cfe76584
Carlos Eduardo Gomes <cesgomes@gmail.com>
2024-10-02 13:24:57 -0300

    Melhorias e comentários do Pieces.APP

--- payment/models.py
+++ payment/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 from store.models import Product
+
+# Modelo para endereços de envio
+
+
 class ShippingAddress(models.Model):
-    user=models.ForeignKey(User,on_delete=models.CASCADE, null=True, blank=True)
-    shipping_full_name=models.CharField(max_length=255)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, blank=True)
+    shipping_full_name = models.CharField(max_length=255)
     shipping_email = models.EmailField(max_length=255)
     shipping_address1 = models.CharField(max_length=255)
     shipping_address2 = models.CharField(max_length=255, null=True, blank=True)
@@ -11,37 +16,39 @@ class ShippingAddress(models.Model):
     shipping_state = models.CharField(max_length=50, null=True, blank=True)
     shipping_zipcode = models.CharField(max_length=20, null=True, blank=True)
     shipping_country = models.CharField(max_length=100)
-    
-    #Dont plurarize address
+
     class Meta:
+        # Não pluralizar o nome do modelo
         verbose_name_plural = "Shipping Address"
-        
+
     def __str__(self):
-        return f'Shipping Address - {str(self.id)}'
-    
-# Create order Model (header)
+        return f'Shipping Address - {self.id}'
+
+# Modelo para pedidos
+
+
 class Order(models.Model):
-    #Foreign key
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, blank=True)
     full_name = models.CharField(max_length=255)
     email = models.EmailField(max_length=255)
     shipping_address = models.TextField(max_length=1000)
     amount_paid = models.DecimalField(max_digits=7, decimal_places=2)
     date_ordered = models.DateField(auto_now_add=True)
-    
+
     def __str__(self):
-        return f'Order - {str(self.id)}'
-    
-# Create Order Items Model
+        return f'Order - {self.id}'
+
+# Modelo para itens do pedido
+
+
 class OrderItem(models.Model):
-    #Foreign keys
     order = models.ForeignKey(Order, on_delete=models.CASCADE, null=True)
     product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, blank=True)
     quantity = models.PositiveSmallIntegerField(default=1)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    
-    def  __str__(self):
-        return f'Order Item - {str(self.id)}'
-    
+
+    def __str__(self):
+        return f'Order Item - {self.id}'
--- payment/urls.py
+++ payment/urls.py
@@ -4,8 +4,13 @@ from . import views
 # Definindo as URLs do aplicativo
 urlpatterns = [
     # URL para a página de sucesso do pagamento
+    # Esta URL mapeia para a função 'payment_success' na views.py
+    # Quando o usuário acessa 'payment_success/', a função 'payment_success' será chamada
     path('payment_success/', views.payment_success, name="payment_success"),
 
     # URL para a página de checkout
+    # Esta URL mapeia para a função 'checkout' na views.py
+    # Quando o usuário acessa 'checkout/', a função 'checkout' será chamada
     path('checkout/', views.checkout, name="checkout"),
 ]
+
