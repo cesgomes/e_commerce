e67bead2958e54feab2ee9cd2ddc5673b7a6cf49
Carlos Eduardo Gomes <cesgomes@gmail.com>
2024-10-02 00:35:04 -0300

    Modelo dos itens dos pedidos

--- payment/models.py
+++ payment/models.py
@@ -33,3 +33,15 @@ class Order(models.Model):
         return f'Order - {str(self.id)}'
     
 # Create Order Items Model
+class OrderItem(models.Model):
+    #Foreign keys
+    user = models.ForeignKey(Order, on_delete=models.CASCADE, null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+
+    quantity = models.PositiveSmallIntegerField(default=1)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    
+    def  __str__(self):
+        return f'Order Item - {str(self.id)}'
+    
