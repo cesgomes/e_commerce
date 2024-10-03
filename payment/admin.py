e7c5f3aa2bc23289d7738e8d4297797d904987fd
Carlos Eduardo Gomes <cesgomes@gmail.com>
2024-10-03 11:01:10 -0300

    Adicao dos itens da ordem, na admin page

--- payment/admin.py
+++ payment/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from .models import ShippingAddress, Order, OrderItem
+from django.contrib.auth.models import User
 
 #Register model in admin section
 admin.site.register(ShippingAddress)
 admin.site.register(Order)
 admin.site.register(OrderItem)
+
+class OrderItemInline(admin.StackedInline):
+    model=OrderItem
+    
+class OrderAdmin(admin.ModelAdmin):
+    model = Order
+    inlines=[OrderItemInline]
+    
+admin.site.unregister(Order)
+admin.site.register(Order, OrderAdmin)
